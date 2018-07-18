d2797076a66e1ca666b262723702453203eb2709
Reginald Johnson <reginald.johnson@gmail.com>
2018-07-18 06:15:47 -0400

    confirming build runs

--- packages/aws-amplify-react/src/Auth/Provider/withFacebook.jsx
+++ packages/aws-amplify-react/src/Auth/Provider/withFacebook.jsx
@@ -51,6 +51,7 @@ export default function withFacebook(Comp) {
             const fb = window.FB;
             fb.api('/me', 'GET', {fields: 'first_name,last_name,name,id,picture.width(150).height(150)'}, response => {
                 debugger;
+                console.log("in facebook get");
                 let user = {
                     name: response.name
                 }
